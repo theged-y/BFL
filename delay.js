fd3bd612b6c81332e68a914d591ebd1605940829
theged-y <torchmarketingby.yuchen@gmail.com>
2024-10-16 11:50:10 +0800

    Create delay.js

---
+++ delay.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("hide").style.display = "block";
+  }, 1320000);
+});
+
+function clickBank() {
+  window.location.href =
+    "https://torchged.pay.clickbank.net/?cbitems=4&cbfid=57413";
+}
